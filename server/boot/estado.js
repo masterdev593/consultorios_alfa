72734b9517db06804408ee5ec185050471e00c22
Vladimir Rodríguez <fizzvr@gmail.com>
2019-05-06 12:40:47 -0500

    > Add fecha > 📲 Developer
    [Fix: ]

--- server/boot/estado.js
+++ server/boot/estado.js
@@ -1,7 +1,9 @@
 'use strict';
+const moment = require('moment');
 
 module.exports = function(app) {
+  const fecha = moment().format('MMMM Do YYYY, h:mm:ss a');
   app.get('/estado', function(req, res) {
-    res.json({corriendo: true});
+    res.json({fecha: fecha, corriendo: true});
   });
 };
