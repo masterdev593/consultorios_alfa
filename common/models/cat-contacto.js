2bbecbb8035fd6d8713b7df0cb4a0dbd36e1c4c7
Vladimir Rodríguez <fizzvr@gmail.com>
2019-05-13 15:54:52 -0500

    > Validación del campo email con una exp regular > 📲 Developer
    [Fix: ]

--- common/models/cat-contacto.js
+++ common/models/cat-contacto.js
@@ -1,5 +1,6 @@
 'use strict';
+const re = /\S+@\S+\.\S+/;
 
 module.exports = function(Catcontacto) {
-
+  Catcontacto.validatesFormatOf('email', {with: re, message: 'Debe ingresar un email válido'});
 };
